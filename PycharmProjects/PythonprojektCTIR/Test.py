04ed473e86b6961525012a45aa036785dae3a9b8
Grisenii <oscar.wissen@gmail.com>
2020-05-13 19:11:35 +0200

    Testar

---
+++ PycharmProjects/PythonprojektCTIR/Test.py
@@ -0,0 +1,11 @@
+
+from tkinter import *
+import pandas as pd
+
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+#testkommentar
